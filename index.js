105e91616b7c75e3a1e322867479c17d161eb670
areebaasim01 <161856617+areebaasim01@users.noreply.github.com>
2024-04-17 18:01:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// 21.	They think of something you could store in a TypeScript Object. Write a program that creates Objects 
+// containing these items.
+Object.defineProperty(exports, "__esModule", { value: true });
+let person = { name: "Ahsen", fname: "Areeba", age: 18 };
+console.log(person);
---
+++ index.ts
@@ -0,0 +1,10 @@
+// 21.	They think of something you could store in a TypeScript Object. Write a program that creates Objects 
+// containing these items.
+
+
+
+
+let person:{name:string, fname:string, age:number} = {name: "Ahsen", fname: "Areeba", age: 18}
+
+
+ console.log(person);
